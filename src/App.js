49c5e30c9934837154146e0e2d742af810cacda3
Sanny Kumar <sannykumar085@gmail.com>
2021-04-24 14:04:35 +0530

    Product in Cart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+import { toast } from 'react-toastify';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = item => {
+    
+    const isAlreadyAdded = cartItem.findIndex( function(array){
+          return array.id === item.id;
+    });
+    if(isAlreadyAdded !== -1) {
+      toast("Already Added In Cart", {
+        type : "error"
+      });
+    }
+    setCartItem([...cartItem, item]);
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+         Ecommerce
+        
       </header>
     </div>
   );
