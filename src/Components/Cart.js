9d39d287d501c5645865a21af54b2c5ac2a27802
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-29 19:47:36 +0530

    Add files via upload

---
+++ src/Components/Cart.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { Container, ListGroup, ListGroupItem, Button, CardHeader, CardBody, Card, CardFooter, Col, Row } from "reactstrap";
+
+const Cart = ({cartItem, removeItem, buyNow }) => {
+    let amount = 0;
+    
+    cartItem.forEach( item => {
+        amount = parseFloat(amount) +parseFloat(item.productPrice);
+    } )
+
+    return (
+        <Container fluid>
+          <h1 className="text-success">Your Cart</h1>
+          <ListGroup>
+          {cartItem.map( item => (
+             <ListGroupItem key={item.id}>
+              <Row>
+                  <Col>
+                      <img src={item.tinyImage} height="60"/>
+                  </Col>
+                  <Col className="text-center">
+                       <div className="text-primary">
+                            {item.productName}
+                       </div>
+                    <span>Price : - {item.productPrice}</span>
+                    <Button color="danger" onClick={ ()=>{removeItem(item)} }>Remove</Button>
+                  </Col>
+              </Row>
+             </ListGroupItem>
+          ))}
+          </ListGroup>
+          
+        
+        </Container>
+    )
+}
+
+export default Cart
