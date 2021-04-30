3c9597d8f9fe593372cd981204ee04cc16b8e57e
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-30 12:50:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,35 +14,40 @@ function App() {
   const [cartItem, setCartItem] = useState([]);
 
   const addInCart = item => {
-    
-    const isAlreadyAdded = cartItem.findIndex( function(array) {
-          return array.id === item.id;
-    });
-    if(isAlreadyAdded !== -1) {
-      toast("Already Added In Cart", {
-        type : "error"
-      });
-    }
-    setCartItem([...cartItem, item]);
-};
+     const isAlreadyAdded = cartItem.findIndex( function(array) {
+           return array.id === item.id;
+     });
+     if(isAlreadyAdded !== -1) {
+       toast("Already Added In Cart", {
+         type : "error"
+       });
+       return;
+     }
+     setCartItem([...cartItem, item]);
+     console.log(cartItem);
+  };
+
   const buyNow = () => {
-    setCartItem([]);
-    toast("Purchased Successfully", {type: "success"}); 
-}; 
+     setCartItem([]);
+     toast("Purchased Successfully", {type: "success"}); 
+  }; 
+
   const removeItem = item => {
-    setCartItem(cartItem.filter( signleItem => signleItem.id !== item.id ));
-};
+     setCartItem(cartItem.filter( signleItem => signleItem.id !== item.id ));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Container fluid>
+        <ToastContainer/>
           <Row>
             <Col md="8">
                Ecommerce Functionality(Add)
                <BuyPage addInCart={addInCart} />
             </Col>
             <Col md="4">
-               <Cart cartItem={cartItem} removeItem={removeItem} />
+               <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
             </Col>
           </Row>
         </Container>
