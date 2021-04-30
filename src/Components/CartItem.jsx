75d0547079cb208f63964fc95a84567eb407e271
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-30 12:52:01 +0530

    Update CartItem.jsx

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -12,7 +12,6 @@ const CartItem = ({product1, addInCart}) => {
               <CardImg top height="130"  width="100%" src={product1.smallImage} alt={product1.productName} />
               <CardBody className="text-center">
                 <CardTitle className="text-dark">{ product1.productName }</CardTitle>
-        
                 <CardText className="text-secondary">Price: RS { product1.productPrice}</CardText>
                 <Button color="success" onClick={()=>{addInCart(product1)}}>Buy Now</Button>
               </CardBody>
