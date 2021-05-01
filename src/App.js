acbdeb33595af5d632d48531ac16b5b4b5b8d75f
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-05-01 16:37:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 import Cart from './Components/Cart';
 
 function App() {
+
   const [cartItem, setCartItem] = useState([]);
 
   const addInCart = item => {
@@ -26,12 +27,10 @@ function App() {
      setCartItem([...cartItem, item]);
      console.log(cartItem);
   };
-
   const buyNow = () => {
      setCartItem([]);
      toast("Purchased Successfully", {type: "success"}); 
   }; 
-
   const removeItem = item => {
      setCartItem(cartItem.filter( signleItem => signleItem.id !== item.id ));
   };
@@ -44,16 +43,17 @@ function App() {
           <Row>
             <Col md="8">
                Ecommerce Functionality(Add)
-               <BuyPage addInCart={addInCart} />
+               <BuyPage addInCart={addInCart}/>
             </Col>
             <Col md="4">
-               <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+               <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow}/>
             </Col>
           </Row>
         </Container>
       </header>
     </div>
   );
+
 }
 
 export default App;
