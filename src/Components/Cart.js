5869a08b8ab5d7ee25ef44ac57abf60a8701c30b
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-05-01 16:37:43 +0530

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Container, ListGroup, ListGroupItem, Button, CardHeader, CardBody, Card, CardFooter, Col, Row } from "reactstrap";
+import { Container, ListGroup, ListGroupItem, Button, CardHeader, CardBody, Card, Col, Row } from "reactstrap";
 
-const Cart = ({cartItem, removeItem, buyNow }) => {
-    let amount = 0;
+const Cart = ({ cartItem, removeItem, buyNow }) => {
+    const [limit, setLimit] = useState(1);
     
-    cartItem.forEach( item => {
-        amount = parseFloat(amount) +parseFloat(item.productPrice);
-    } )
-
-    return (
-        
-        <Container fluid>
+    let amount = 0; 
+    let totalamount = 0; 
+    
+    cartItem.map( item => {
+        amount = parseFloat(amount) + parseFloat(item.productPrice);
+        totalamount = parseFloat(amount) * parseFloat(limit);
+        console.log("Quantity is from gloabl: "+ limit);
+    });
+    
+return (
+    <Container fluid>
         <h1 className="text-success">Your Cart</h1>
           {
             cartItem.length >=1 ? (
@@ -20,36 +24,49 @@ const Cart = ({cartItem, removeItem, buyNow }) => {
                        <span className="text-warning">Total Price : </span>  
                     </CardHeader>
                     <CardBody>
-                        <h6 className="text-secondary">Your Amount for {cartItem.length} Product is {amount}</h6>
+                        <h6 className="text-secondary">Your Amount for <span className="text-dark">{cartItem.length} </span> 
+                         Product is <span className="text-dark">{totalamount}</span></h6>
                         <Button color="warning" onClick={buyNow}>Pay Now</Button>
                     </CardBody>
                 </Card>
             ) : (
-                 <h5 className="text-warning">Cart is empty</h5>
-            )
-          }
+                <Card>
+                    <CardBody>
+                    <h5 className="text-warning">Cart is empty</h5> 
+                    </CardBody>
+                    
+                </Card>
+          )
+        }
           <ListGroup>
-          {cartItem.map( item => (
+         { cartItem.map( item => (
              <ListGroupItem key={item.id}>
               <Row>
                   <Col>
-                      <img src={item.tinyImage} height="60"/>
+                      <img src={item.tinyImage} height="58" height="70" alt={item.productName} className="img-fluid mx-auto d-block" />
                   </Col>
-                  <Col >
+                  <Col>
                        <div className="text-primary">
                             {item.productName}
                        </div>
-                    <span className="text-dark">Price : {item.productPrice}</span>
+                    <div className="text-dark">Price : {item.productPrice}</div>
+                    <div className="text-dark">Quantity :  &nbsp;
+                    
+                        <span className="h4 cursor" onClick={()=> item.quantity > 1 ? setLimit(limit-1) : setLimit(1)}><strong>-</strong></span>
+                         &nbsp;&nbsp; { item.quantity = limit } &nbsp;&nbsp;
+                        <span className="h4 cursor" onClick={()=> item.quantity < 30 ? setLimit(limit+1) : alert('Maximum product added')}><strong>+</strong></span>
+                    
+                    </div>
                     <Button color="danger"className="btn-sm"  onClick={ ()=>{removeItem(item)} }>Remove</Button>
                   </Col>
               </Row>
              </ListGroupItem>
           ))}
           </ListGroup>
-        
-         
-        </Container>
-    )
+    </Container>
+  )
 }
 
+
 export default Cart;
+
