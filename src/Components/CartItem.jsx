ccbfc3b37d960bc0f2008f6dec60be060f46569c
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-05-05 08:11:05 +0530

    Update CartItem.jsx

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -8,16 +8,15 @@ import {
 const CartItem = ({product1, addInCart}) => {
     return (
         <div>
-            <Card>
-              <CardImg top height="130"  width="100%" src={product1.smallImage} alt={product1.productName} />
+          <Card>
+               <CardImg top height="130"  width="100%" src={product1.smallImage} alt={product1.productName} />
               <CardBody className="text-center">
-                <CardTitle className="text-dark">{ product1.productName }</CardTitle>
-                <CardText className="text-secondary">Price: RS { product1.productPrice}</CardText>
-                <Button color="success" onClick={()=>{addInCart(product1)}}>Buy Now</Button>
+                 <CardTitle className="text-dark">{ product1.productName }</CardTitle>
+                 <CardText className="text-secondary">Price: RS { product1.productPrice}</CardText>
+                 <Button color="success" onClick={()=>{addInCart(product1)}}>Buy Now</Button>
               </CardBody>
-            </Card>
+          </Card>
         </div>
     )
 }
-
 export default CartItem;
