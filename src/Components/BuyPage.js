44a754972c9aac08671c066c4a7ea7f460682a7d
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-05-05 08:13:17 +0530

    Update BuyPage.js

--- src/Components/BuyPage.js
+++ src/Components/BuyPage.js
@@ -5,26 +5,23 @@ import { random, commerce, datatype } from "faker";
 import { Container, Col, Row } from "reactstrap";
 
 import CartItem from "./CartItem";
+
+const apiKey = "INSET_YOUR_KEY_HERE";
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+
 const localurl = "https://api.npoint.io/ba9493f750096240eb35";
 
 const BuyPage = ({ addInCart }) => {
   const [product, setProduct] = useState([]);
-
-    // const fetchPhotos = async () => {
-    //   const response = await Axios.get(url, {
-    //     header: {
-    //       Authorization: apiKey
-    //     }
-    //   });
-
+ 
   const fetchPhotos = async () => {
     try{
      const response = await Axios.get(localurl);
     console.log(response);
     // Destructuring from object
-     const {photos} = response.data;
+    const {photos} = response.data;
    
-     const allProduct = photos.map( photo => ({
+    const allProduct = photos.map( photo => ({
        smallImage: photo.src.medium,
        tinyImage: photo.src.tiny,
        productName: random.word(),
@@ -33,7 +30,7 @@ const BuyPage = ({ addInCart }) => {
        quantity: 1
      }) );
    
-      setProduct(allProduct); 
+     setProduct(allProduct); 
     }
     catch(err){
         console.log("Catch:"+err)
